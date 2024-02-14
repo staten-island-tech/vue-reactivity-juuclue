574582723acdeb49a50bca668b97cc3cd428c710
juuclue <144699439+juuclue@users.noreply.github.com>
2024-02-14 10:14:48 -0500

    e

--- angela-yi/src/views/HomeView.vue
+++ angela-yi/src/views/HomeView.vue
@@ -1,16 +1,16 @@
 
 <template>
   <div class ="cards">
-        <DestCard
+        <card2
       v-for="supply in supplys"
       :key="supply.name"
-      :Supply="supplys"
+      :Supply="supply"
     />
   </div>
 </template>
 
 <script setup>
-import DestCard from "@/components/DestCard.vue";
+import card2 from "@/components/DestCard.vue";
 const supplys = [
  {
   name: "Nori(shredded)",
@@ -42,15 +42,15 @@ const supplys = [
  },
  {
   name: "Soba noodles",
-  img: "https://www.markys.com/media/catalog/product/cache/b0d56cdde5a8ef6c30b52cc73236630f/0/1/014202-01_v0ec04odvsbf18ed.jpg",
+  img: "https://www.chopstickchronicles.com/wp-content/uploads/2021/03/Soba-Noodle-10.webp",
  },
  {
   name: "Zucchini noodles",
-  img: "https://www.markys.com/media/catalog/product/cache/b0d56cdde5a8ef6c30b52cc73236630f/0/1/014202-01_v0ec04odvsbf18ed.jpg",
+  img: "https://www.wholesomeyum.com/wp-content/uploads/2021/06/wholesomeyum-Zucchini-Noodles-Recipe-Zoodles-15.jpg",
  },
  {
   name: "brown rice",
-  img: "https://www.markys.com/media/catalog/product/cache/b0d56cdde5a8ef6c30b52cc73236630f/0/1/014202-01_v0ec04odvsbf18ed.jpg",
+  img: "https://media.post.rvohealth.io/wp-content/uploads/2023/09/brown-rice-header.jpg",
  },
  {
   name: "negitoro",
@@ -58,71 +58,71 @@ const supplys = [
  },
  {
   name: "Spring mix",
-  img: "https://www.markys.com/media/catalog/product/cache/b0d56cdde5a8ef6c30b52cc73236630f/0/1/014202-01_v0ec04odvsbf18ed.jpg",
+  img: "https://foolproofliving.com/wp-content/uploads/2022/03/Spring-Mix-Salad-Recipe.jpg",
  },
  {
   name: "spicy tuna",
-  img: "https://www.markys.com/media/catalog/product/cache/b0d56cdde5a8ef6c30b52cc73236630f/0/1/014202-01_v0ec04odvsbf18ed.jpg",
+  img: "https://www.savorysweetspoon.com/wp-content/uploads/2023/07/Spicy-Tuna-3-960x1200.jpg",
  },
  {
   name: "Spicy salmon",
-  img: "https://www.markys.com/media/catalog/product/cache/b0d56cdde5a8ef6c30b52cc73236630f/0/1/014202-01_v0ec04odvsbf18ed.jpg",
+  img: "https://www.tastingtable.com/img/gallery/spicy-honey-glazed-salmon-recipe/l-intro-1644519469.jpg",
  },
  {
   name: "Shrimp",
-  img: "https://www.markys.com/media/catalog/product/cache/b0d56cdde5a8ef6c30b52cc73236630f/0/1/014202-01_v0ec04odvsbf18ed.jpg",
+  img: "https://www.cookingforkeeps.com/wp-content/uploads/2023/03/Shrimp-Poke-Bowls-Web-3-1024x683.jpg.webp",
  },
  {
   name: "chicken",
-  img: "https://www.markys.com/media/catalog/product/cache/b0d56cdde5a8ef6c30b52cc73236630f/0/1/014202-01_v0ec04odvsbf18ed.jpg",
+  img: "https://www.foodservicedirect.com/media/catalog/product/1/0/10038034052495_2.jpg?width=1200&height=1200&quality=85&fit=bounds",
  },
  {
   name: "organic tofu",
-  img: "https://www.markys.com/media/catalog/product/cache/b0d56cdde5a8ef6c30b52cc73236630f/0/1/014202-01_v0ec04odvsbf18ed.jpg",
+  img: "https://hurrythefoodup.com/wp-content/uploads/2021/12/sesame-tofu-vegan-poke-bowl-in-progress.jpg",
  },
  {
   name: "yellowtail",
-  img: "https://www.markys.com/media/catalog/product/cache/b0d56cdde5a8ef6c30b52cc73236630f/0/1/014202-01_v0ec04odvsbf18ed.jpg",
+  img: "https://thetopmeal.com/wp-content/uploads/2022/11/hamachi-poke-3.jpg",
  },
  {
   name: "Unagi(EEL)",
-  img: "https://www.markys.com/media/catalog/product/cache/b0d56cdde5a8ef6c30b52cc73236630f/0/1/014202-01_v0ec04odvsbf18ed.jpg",
+  img: "https://cdn.media.amplience.net/i/japancentre/recipe-1434-unagi-don-grilled-eel-rice-bowl/recipe-1434-unagi-don-grilled-eel-rice-bowl?$poi$&w=700&h=410&sm=c&fmt=auto",
  },
  {
   name: "grilled salmon",
-  img: "https://www.markys.com/media/catalog/product/cache/b0d56cdde5a8ef6c30b52cc73236630f/0/1/014202-01_v0ec04odvsbf18ed.jpg",
+  img: "https://feelgoodfoodie.net/wp-content/uploads/2021/06/how-to-grill-salmon-8.jpg",
  },
  {
   name: "teriyaki beef",
-  img: "https://www.markys.com/media/catalog/product/cache/b0d56cdde5a8ef6c30b52cc73236630f/0/1/014202-01_v0ec04odvsbf18ed.jpg",
+  img: "https://www.slimmingeats.com/blog/wp-content/uploads/2016/06/beef-teriyaki-4-1.jpg",
  },
  {
   name: "Cilantro",
-  img: "https://www.markys.com/media/catalog/product/cache/b0d56cdde5a8ef6c30b52cc73236630f/0/1/014202-01_v0ec04odvsbf18ed.jpg",
+  img: "https://healthiersteps.com/wp-content/uploads/2021/11/cilantro-bunch.jpg",
  },
  {
   name: "hijiki seaweed",
-  img: "https://www.markys.com/media/catalog/product/cache/b0d56cdde5a8ef6c30b52cc73236630f/0/1/014202-01_v0ec04odvsbf18ed.jpg",
+  img: "https://www.maindish.in/wp-content/uploads/2020/09/hijiki_main2.jpg",
  },
  {
   name: "Jalapeno",
-  img: "https://www.markys.com/media/catalog/product/cache/b0d56cdde5a8ef6c30b52cc73236630f/0/1/014202-01_v0ec04odvsbf18ed.jpg",
+  img: "https://measianfoods.com/cdn/shop/products/jalapeno.png?v=1646605821",
  },
  {
   name: "sweet corn",
-  img: "https://www.markys.com/media/catalog/product/cache/b0d56cdde5a8ef6c30b52cc73236630f/0/1/014202-01_v0ec04odvsbf18ed.jpg",
+  img: "https://www.jiomart.com/images/product/original/590001511/sweet-corn-shelled-200-g-pack-product-images-o590001511-p590335340-0-202203171044.jpg?im=Resize=(1000,1000)",
  },
  {
   name: "Sweet onion",
-  img: "https://www.markys.com/media/catalog/product/cache/b0d56cdde5a8ef6c30b52cc73236630f/0/1/014202-01_v0ec04odvsbf18ed.jpg",
+  img: "https://www.seriouseats.com/thmb/6lQgiWc9s0tLibRrf_85CJY9mco=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__images__2016__06__20160608-poke-tuna-hamachi-octopus-salmon-hawaii-recipe-02-fe42fe8c18e64a72b85c4cb2f11b84cc.jpg",
  },
  {
   name: "carrot",
-  img: "https://www.markys.com/media/catalog/product/cache/b0d56cdde5a8ef6c30b52cc73236630f/0/1/014202-01_v0ec04odvsbf18ed.jpg",
+  img: "https://hentick.com/cdn/shop/files/carrots_600x.jpg?v=1686035952",
  },
  {
   name: "Cilantro Ranch",
-  img: "https://www.markys.com/media/catalog/product/cache/b0d56cdde5a8ef6c30b52cc73236630f/0/1/014202-01_v0ec04odvsbf18ed.jpg",
+  img: "https://www.chelseasmessyapron.com/wp-content/uploads/2022/08/Healthy-Cilantro-Lime-Dressing-1.jpeg",
  },
  {
   name: "House Poke sauce",
