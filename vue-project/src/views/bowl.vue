b6cfac34ea1b95a02a191a1fbc464ed579015768
juuclue <144699439+juuclue@users.noreply.github.com>
2024-02-26 10:15:28 -0500

    e

--- vue-project/src/views/bowl.vue
+++ vue-project/src/views/bowl.vue
@@ -26,3 +26,34 @@ console.log(bowl);
   }
 }
 </style> -->
+<template>
+  <div class="bowl">
+    <div v-if="bowl.length === 0">
+      <p> no items in bowl</p>
+    </div>
+    <div v-else>
+      <div v-for="(ingredient) in bowl" :key="ingredient.name" class="ingredient">
+      <h3> {{  ingredient.name }}</h3>
+      <img :src="ingredient.img" alt=""/>
+      
+    </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import { defineProps } from 'vue';
+
+const props= defineProps({
+  bowl: array
+})
+</script>
+
+<style scoped>
+.bowl{
+  background-color:rgb(131, 107, 107);
+  width:30%;
+  position:absolute;
+  top:0; right:0;
+  height:889vh;
+}
+</style>
